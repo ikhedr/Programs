36434ff81518a73adb5d74bd62b8fcbc29bee09e
Datta Ikhe <ikhe10dr@gmail.com>
2018-01-31 23:34:53 +0530

    31 Jan 2017
    Python program- Asked in google interview

---
+++ FindSumPairs.py
@@ -0,0 +1,30 @@
+#Given array list, find all the pairs of numbers whose sum equal to given sum
+# Time Complext- O(n)
+# Space Complexity- O(n) 
+
+import sys
+def FindSumPairs(Numbers, requiredSum):
+	tmpbuffer=[]
+	thePairs={}
+	counter=0
+	flg=0
+	complement=0
+	for number in Numbers:
+		complement = requiredSum - number
+		tmplist=[]
+		tmplist.append(number)
+		if(number in tmpbuffer):
+			tmplist.append(complement)
+			thePairs[counter]=tmplist
+			flg=1
+			counter += 1
+		tmpbuffer.append(complement)
+		if(flg==1):
+			tmpbuffer.remove(complement)
+		flg=0
+	return thePairs
+
+
+Numbers=[3,6,2,5,7,4,3] 
+requiredSum = 9
+print(FindSumPairs(Numbers,requiredSum))
